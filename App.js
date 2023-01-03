4f8ed4037d5e2aa302b1033298ca56f584f415f8
Helena Gingembre <e.izotovagingembre@outlook.com>
2023-01-03 14:47:42 +0100

    home navigate

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { Platform } from "react-native";
-import { FormRegistration } from "./Screens/auth/RegistrationScreen";
-import { LoginForm } from "./Screens/auth/LoginScreen";
-import { Home } from "./Pages/Home";
+import { Platform } from 'react-native';
+import { FormRegistration } from './Screens/auth/RegistrationScreen';
+import { LoginForm } from './Screens/auth/LoginScreen';
+import { Home } from './Pages/Home';
 
 const MainStack = createStackNavigator(); // указывает на группу навигаторов
 
 export default function App() {
-  console.log(Platform.OS);
-  return (
-    <NavigationContainer>
-       <MainStack.Navigator>{/* Замена Switch */}
-        <MainStack.Screen name="Registration" component={FormRegistration} />{/* Замена Route */}
-        <MainStack.Screen name="Login" component={LoginForm} />
-        <MainStack.Screen name="Home" component={Home} />
-      </MainStack.Navigator>
-     
-    </NavigationContainer>
+    console.log(Platform.OS);
+    return (
+        <NavigationContainer>
+            <MainStack.Navigator initialRouteName="Home">
+                {/* Замена Switch */}
+                <MainStack.Screen
+                    name="Registration"
+                    component={FormRegistration}
+                />
+                {/* Замена Route */}
+                <MainStack.Screen name="Login" component={LoginForm} />
+                <MainStack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ title: 'Start screen' }}
+                />
+            </MainStack.Navigator>
+        </NavigationContainer>
 
-    // <FormRegistration />
-    // <LoginForm />
-  );
+        // <FormRegistration />
+        // <LoginForm />
+    );
 }
--- Pages/Home.js
+++ Pages/Home.js
@@ -1,19 +1,40 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const Home = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titleHeader}>Публiкацii</Text>
+            {/* //! ------------- контейнер: photoFrame ------------ */}
+            <View style={styles.photoFrame}></View>
+            <Text style={styles.text}>Вже є акаунт? Увійти</Text>
+            <Text style={styles.text}>Вже є акаунт? Увійти</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    titleHeader: {
+        fontWeight: '500',
+        fontSize: 17,
+        lineHeight: 1.29,
+        textAlign: 'center',
+        letterSpacing: -0.408,
+        color: '#212121',
+    },
+    photoFrame: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#F6F6F6',
+        marginTop: -60,
+        borderRadius: 16,
+    },
 });
-
